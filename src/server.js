350a1a1bbe9659ccba0b4bc011b571401894b601
Esdras Gomes <esdrasgoomes@gmail.com>
2021-02-07 13:52:31 -0300

    Configurando e modificando o projeto

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 // Servidor
 const express = require('express')
 const server = express()
-const { pageLanding, pageStudy, pageGiveClasses } = require('./pages')
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require('./pages')
 const nunjucks = require('nunjucks')
 
 // configurar nunjucks (template engine)
@@ -10,11 +10,16 @@ nunjucks.configure('src/views', {
     noCache: true,
 })
 
+// iniciando e configurando o servidor
+server
+// receber os dados do req.body
+.use(express.urlencoded({ extended: true }))
 // configurar servidor e configurar arquivos estáticos (css, scripts, imagens)
-server.use(express.static("public"))
+.use(express.static("public"))
 // rotas da aplicação
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
 // start do servidor
 .listen(5500)
